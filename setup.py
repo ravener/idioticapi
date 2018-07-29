23bada58feb106df67a5e900fff314c0aa89702e
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-07-29 18:25:16 +0300

    Use double quotes REEE

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ description = "A simple idiot's guide api wrapper in python"
 long_description = open("README.md").read()
 version="1.1.0"
 
-packages = ['idioticapi']
+packages = ["idioticapi"]
 
 setuptools.setup(
-    name='idioticapi',
+    name="idioticapi",
     version=version,
     description=description,
     long_description=long_description,
-    url='https://github.com/freetnt5852/idioticapi',
-    author='Free TNT',
-    author_email='darksoulgamer5852@gmail.com',
-    license='MIT',
+    url="https://github.com/freetnt5852/idioticapi",
+    author="Free TNT",
+    author_email="darksoulgamer5852@gmail.com",
+    license="MIT",
     packages=packages,
     include_package_data=True,
-    install_requires=['aiohttp>=2.0.0']
+    install_requires=["aiohttp>=2.0.0"]
 )
